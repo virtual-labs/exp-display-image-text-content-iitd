bd40d7ad0dad4c2718a5041b10d138a9309358c8
NidhiGoyal06 <140578560+NidhiGoyal06@users.noreply.github.com>
2023-09-19 07:25:21 +0530

    Add files via upload

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -5,140 +5,137 @@
 /////////////////////////////////////////////////////////////////////////////
 
 (function() {
-  function buildQuiz() {
-    // we'll need a place to store the HTML output
-    const output = [];
+    function buildQuiz() {
+        // we'll need a place to store the HTML output
+        const output = [];
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // we'll want to store the list of answer choices
-      const answers = [];
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // we'll want to store the list of answer choices
+            const answers = [];
 
-      // and for each available answer...
-      for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
+            // and for each available answer...
+            for (letter in currentQuestion.answers) {
+                // ...add an HTML radio button
+                answers.push(
+                    `<label>
             <input type="radio" name="question${questionNumber}" value="${letter}">
             ${letter} :
             ${currentQuestion.answers[letter]}
           </label>`
-        );
-      }
+                );
+            }
 
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
+            // add this question and its answers to the output
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
         <div class="answers"> ${answers.join("")} </div>`
-      );
-    });
+            );
+        });
 
-    // finally combine our output list into one string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
-  }
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join("");
+    }
 
-  function showResults() {
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll(".answers");
+    function showResults() {
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll(".answers");
 
-    // keep track of user's answers
-    let numCorrect = 0;
+        // keep track of user's answers
+        let numCorrect = 0;
 
-    // for each question...
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+        // for each question...
+        myQuestions.forEach((currentQuestion, questionNumber) => {
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
 
-        // color the answers green
-        //answerContainers[questionNumber].style.color = "lightgreen";
-      } else {
-        // if answer is wrong or blank
-        // color the answers red
-        answerContainers[questionNumber].style.color = "red";
-      }
-    });
+                // color the answers green
+                //answerContainers[questionNumber].style.color = "lightgreen";
+            } else {
+                // if answer is wrong or blank
+                // color the answers red
+                answerContainers[questionNumber].style.color = "red";
+            }
+        });
 
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  }
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
 
-  const quizContainer = document.getElementById("quiz");
-  const resultsContainer = document.getElementById("results");
-  const submitButton = document.getElementById("submit");
-
-
-/////////////////////////////////////////////////////////////////////////////
-
-/////////////////////// Do not modify the above code ////////////////////////
-
-/////////////////////////////////////////////////////////////////////////////
+    const quizContainer = document.getElementById("quiz");
+    const resultsContainer = document.getElementById("results");
+    const submitButton = document.getElementById("submit");
 
 
+    /////////////////////////////////////////////////////////////////////////////
+
+    /////////////////////// Do not modify the above code ////////////////////////
+
+    /////////////////////////////////////////////////////////////////////////////
 
 
 
 
-/////////////// Write the MCQ below in the exactly same described format ///////////////
 
 
-  const myQuestions = [
-    {
-        question: "Q1) What value will print to the console? Please select the closest answer: 
-	                      const a = new Date('2023-01-01') 
-	                      const b = new Date('2023-01-01T00:00') 
-	                      console.log(a.getFullYear(), b.getFullYear()) ",
-        answers: {
-            a: ' 2019 2023',
-            b: ' 2023 2021',
-            c: ' 2023 2023',
-            d: ' 2023 2019'
+    /////////////// Write the MCQ below in the exactly same described format ///////////////
+
+
+    const myQuestions = [{
+            question: "Q1) How does Java Script store dates in objects of Date type? ",
+            answers: {
+                a: "The number of days since January 1st, 1900",
+                b: "The number of seconds since January 1st, 1970",
+                c: "The number of milliseconds since January 1st, 1970",
+                d: "The number of picoseconds since January 1st, 1970"
 
 
 
-                          
+
+            },
+            correctAnswer: 'c'
         },
-        correctAnswer: 'c'
-    },
-    {
-        question: "Q2)  What value will print to the console? Please select the closest answer: 
-	                      const d2 = new Date(2020, 1, 1) 
-	                      console.log(d2) ",
-        answers: {
-            a: ' 2023-02-01',
-            b: ' 2023-01-01',
-            c: ' 2023-03-01',
-            d: ' 2023-04-01'
 
+        {
+            question: "Q2) Which of the ways below is incorrect of instantiating a date? ",
+            answers: {
+                a: "new Date(dateString)",
+                b: "new Date()",
+                c: "new Date(seconds)",
+                d: "new Date(year, month, day, hours, minutes, seconds, milliseconds)"
+
+
+
+            },
+            correctAnswer: 'c'
         },
-        correctAnswer: 'b'
-    },
-   
 
-  ];
+
+    ];
 
 
 
 
-/////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////
 
-/////////////////////// Do not modify the below code ////////////////////////
+    /////////////////////// Do not modify the below code ////////////////////////
 
-/////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////
 
 
-  // display quiz right away
-  buildQuiz();
+    // display quiz right away
+    buildQuiz();
 
-  
-  // on submit, show results
-  submitButton.addEventListener("click", showResults);
+
+    // on submit, show results
+    submitButton.addEventListener("click", showResults);
 })();
 
 
@@ -147,4 +144,3 @@
 /////////////////////// Do not modify the above code ////////////////////////
 
 /////////////////////////////////////////////////////////////////////////////
-
